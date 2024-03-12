2ef7a060ddd7149eec67fc5e8145fad2f5a5441a
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-03-13 00:20:36 +0600

    user's comment designed

--- src/Components/ReplyComment.jsx
+++ src/Components/ReplyComment.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
 import { FaReply, FaTrash, FaPen } from "react-icons/fa";
 
-const ReplyComment = ({ reply }) => {
+const ReplyComment = ({ reply, user }) => {
+  console.log(user);
   const { time, name, to, number, img, comment } = reply;
   return (
     <div className="bg-[#ffffff] p-4 flex  justify-end  items-center gap-x-4 w-[496px] rounded-md">
@@ -25,17 +26,42 @@ const ReplyComment = ({ reply }) => {
             <p className="text-sm text-[#393b41] font-semibold cursor-pointer hover:underline">
               {name}
             </p>
+
+            {/* if user then it will display otherwise not  */}
+            {user && (
+              <p className="text-xs bg-[#5358b6] text-white font-bold px-1 text-center py-[2px] rounded-sm">
+                you
+              </p>
+            )}
             <p className="text-sm text-[#777779] font-semibold">{time}</p>
           </div>
 
           {/* reply button  */}
           <div className="">
-            <p className="flex items-center gap-x-2 text-[#414698] cursor-pointer">
-              <span className="text-xs">
-                <FaReply></FaReply>
-              </span>
-              <span className="text-sm font-semibold">Reply</span>
-            </p>
+            {/* if user then delete and edit otherwise reply options  */}
+            {user ? (
+              <div className="flex gap-x-4">
+                <p className="flex items-center gap-x-2 text-[#ec6266] cursor-pointer">
+                  <span className="text-xs">
+                    <FaTrash></FaTrash>
+                  </span>
+                  <span className="text-sm font-semibold">Delete</span>
+                </p>
+                <p className="flex items-center gap-x-2 text-[#414698] cursor-pointer">
+                  <span className="text-xs">
+                    <FaPen></FaPen>
+                  </span>
+                  <span className="text-sm font-semibold">Edit</span>
+                </p>
+              </div>
+            ) : (
+              <p className="flex items-center gap-x-2 text-[#414698] cursor-pointer">
+                <span className="text-xs">
+                  <FaReply></FaReply>
+                </span>
+                <span className="text-sm font-semibold">Reply</span>
+              </p>
+            )}
           </div>
         </div>
 
