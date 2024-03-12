dd173dce4f27b002b1adc7c161e86723f01d4b31
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-03-12 22:11:28 +0600

    created comment box

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import CommentBox from "./Components/CommentBox";
 import MainComment from "./Components/MainComment";
 
 const App = () => {
   return (
-    <div className="bg-[#f5f6fa] h-screen flex justify-center items-center">
-      <div className="border border-red-500"></div>
-
-      <MainComment></MainComment>
+    <div className="bg-[#f5f6fa] h-screen flex items-center justify-center">
+      <div className=" flex flex-col gap-y-4">
+        <MainComment></MainComment>
+        <MainComment></MainComment>
+        <CommentBox></CommentBox>
+      </div>
     </div>
   );
 };
---
+++ src/Components/CommentBox.jsx
@@ -0,0 +1,21 @@
+const CommentBox = () => {
+  return (
+    <div className="bg-[#ffffff] p-4 flex justify-between gap-x-2">
+      <img
+        className="h-[26px] w-[26px] rounded-[50%] object-cover"
+        src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+      <input
+        type="text"
+        placeholder="Add a comment..."
+        className="border border-gray-200 rounded-md w-[75%]  px-4 py-2  placeholder:text-gray-500 placeholder:font-semibold placeholder:text-sm"
+      />
+      <button className="bg-[#5358b6] text-white rounded-md  px-5 py-2 font-semibold text-sm">
+        SEND
+      </button>
+    </div>
+  );
+};
+
+export default CommentBox;
